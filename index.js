e74de272d2182d1565a61ee6291fa74c76adfb68
Sam Knowles <samuelkcknowles@gmail.com>
2019-08-22 19:07:13 -0600

    set up backend to work with Heroku, like a boss

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
+require("dotenv").config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const employeeRoute = require('./routes/employee');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const PORT = process.env.PORT || 5001;
+const config = require('./config');
+const path = require("path");
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/employees', (err) => {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/employees",
+(err) => {
     if (err) throw err;
-    console.log('Congratulations! You are connected to the backend!');
-})
+    console.log("Connected to Mongo");
+});
 
-//port
-const config = require('./config')
-
-//middlewre
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-//routes
 app.use('/employee', employeeRoute);
 
-//runapp
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(config.port, () => {
     console.log('Listening on port ' + config.port)
 })
